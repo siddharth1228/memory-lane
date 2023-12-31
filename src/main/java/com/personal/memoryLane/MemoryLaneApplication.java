2cc9f66f76e485bdda0de16bb3d2508326a1c0f2
Siddharth Dahiya <siddharth.dahiya@jublfood.com>
2023-12-31 23:30:55 +0530

    project setup

---
+++ src/main/java/com/personal/memoryLane/MemoryLaneApplication.java
@@ -0,0 +1,13 @@
+package com.personal.memoryLane;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoryLaneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoryLaneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/personal/memoryLane/MemoryLaneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.memoryLane;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoryLaneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
