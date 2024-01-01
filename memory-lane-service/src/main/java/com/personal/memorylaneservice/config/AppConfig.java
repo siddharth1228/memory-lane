6ded4ddd225bbbf9b2eaf8b7ec281026f81cf0b5
Siddharth Dahiya <siddharth.dahiya@jublfood.com>
2024-01-01 16:48:51 +0530

    multi module setup

--- memory-lane-service/src/main/java/com/personal/memorylaneservice/config/AppConfig.java
+++ memory-lane-service/src/main/java/com/personal/memorylaneservice/config/AppConfig.java
@@ -1,6 +1,5 @@
 package com.personal.memorylaneservice.config;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
